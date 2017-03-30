a3f3b967244df1f33c70a6654b744ada2c268abc
David Bates <David Bates>
2017-03-30 12:39:15 -0400

    Add project files.

---
+++ BrainTease_3_30_17/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrainTease_3_30_17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsPalendrome(input_txt.Text))
+            {
+                answer.Content = "Yup";
+            }
+            else
+            {
+                answer.Content = "Nope";
+            }
+        }
+
+        private Boolean IsPalendrome(String val)
+        {
+            var q = val.GroupBy(x => x)
+            .Select(g => new { Value = g.Key, Count = g.Count() })
+            .Count(x=>IsOdd(x.Count));
+           
+            return !(q > 1);
+        }
+
+        private Boolean IsOdd(int val)
+        {
+            return val % 2 != 0;
+        }
+        
+    }
+}
